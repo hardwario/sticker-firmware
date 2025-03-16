9fd622f03beb4f6fb3ebf65a79b368e63e441731
Pavel Hübner <pavel.hubner@hardwario.com>
2025-03-16 14:53:56 +0100

    Refactor alarm functionality

--- src/app_alarm.c
+++ src/app_alarm.c
@@ -18,91 +18,100 @@
 
 LOG_MODULE_REGISTER(app_alarm, LOG_LEVEL_DBG);
 
-#define ALARM_TEMPERATURE_THR_LO 18.f
-#define ALARM_TEMPERATURE_THR_HI 35.f
-#define ALARM_TEMPERATURE_HST    0.5f
-
-#define ALARM_EXT_TEMPERATURE_1_THR_LO 2.f
-#define ALARM_EXT_TEMPERATURE_1_THR_HI 8.f
-#define ALARM_EXT_TEMPERATURE_1_HST    0.5f
-
-#define ALARM_EXT_TEMPERATURE_2_THR_LO 2.f
-#define ALARM_EXT_TEMPERATURE_2_THR_HI 8.f
-#define ALARM_EXT_TEMPERATURE_2_HST    0.5f
+#define ALARM_INT_TEMP_THR_LO   (CONFIG_APP_ALARM_INT_TEMP_THR_LO_MUL_100 / 100.f)
+#define ALARM_INT_TEMP_THR_HI   (CONFIG_APP_ALARM_INT_TEMP_THR_HI_MUL_100 / 100.f)
+#define ALARM_INT_TEMP_HST      (CONFIG_APP_ALARM_INT_TEMP_HST_MUL_100 / 100.f)
+#define ALARM_EXT_TEMP_1_THR_LO (CONFIG_APP_ALARM_EXT_TEMP_1_THR_LO_MUL_100 / 100.f)
+#define ALARM_EXT_TEMP_1_THR_HI (CONFIG_APP_ALARM_EXT_TEMP_1_THR_HI_MUL_100 / 100.f)
+#define ALARM_EXT_TEMP_1_HST    (CONFIG_APP_ALARM_EXT_TEMP_1_HST_MUL_100 / 100.f)
+#define ALARM_EXT_TEMP_2_THR_LO (CONFIG_APP_ALARM_EXT_TEMP_2_THR_LO_MUL_100 / 100.f)
+#define ALARM_EXT_TEMP_2_THR_HI (CONFIG_APP_ALARM_EXT_TEMP_2_THR_HI_MUL_100 / 100.f)
+#define ALARM_EXT_TEMP_2_HST    (CONFIG_APP_ALARM_EXT_TEMP_2_HST_MUL_100 / 100.f)
 
 bool app_alarm_is_active(void)
 {
-	static bool alarm_temperature = false;
+	static bool alarm_int_temp = false;
 
 	if (isnan(g_app_sensor_data.temperature)) {
-		alarm_temperature = false;
-	} else if (alarm_temperature) {
+		alarm_int_temp = false;
+	} else if (alarm_int_temp) {
 		if (g_app_sensor_data.temperature >
-			    (ALARM_TEMPERATURE_THR_LO + ALARM_TEMPERATURE_HST) &&
+			    (ALARM_INT_TEMP_THR_LO + ALARM_INT_TEMP_HST) &&
 		    g_app_sensor_data.temperature <
-			    (ALARM_TEMPERATURE_THR_HI - ALARM_TEMPERATURE_HST)) {
-			LOG_INF("Deactivated alarm for temperature");
+			    (ALARM_INT_TEMP_THR_HI - ALARM_INT_TEMP_HST)) {
+			LOG_INF("Deactivated alarm for internal temperature");
 
-			alarm_temperature = false;
+			alarm_int_temp = false;
 		}
 	} else {
 		if (g_app_sensor_data.temperature <
-			    (ALARM_TEMPERATURE_THR_LO - ALARM_TEMPERATURE_HST) ||
+			    (ALARM_INT_TEMP_THR_LO - ALARM_INT_TEMP_HST) ||
 		    g_app_sensor_data.temperature >
-			    (ALARM_TEMPERATURE_THR_HI + ALARM_TEMPERATURE_HST)) {
-			LOG_INF("Activated alarm for temperature");
+			    (ALARM_INT_TEMP_THR_HI + ALARM_INT_TEMP_HST)) {
+			LOG_INF("Activated alarm for internal temperature");
 
-			alarm_temperature = true;
+			alarm_int_temp = true;
 		}
 	}
 
-	static bool alarm_ext_temperature_1 = false;
+	static bool alarm_ext_temp_1 = false;
 
 	if (isnan(g_app_sensor_data.ext_temperature_1)) {
-		alarm_ext_temperature_1 = false;
-	} else if (alarm_ext_temperature_1) {
+		alarm_ext_temp_1 = false;
+	} else if (alarm_ext_temp_1) {
 		if (g_app_sensor_data.ext_temperature_1 >
-			    (ALARM_EXT_TEMPERATURE_1_THR_LO + ALARM_EXT_TEMPERATURE_1_HST) &&
+			    (ALARM_EXT_TEMP_1_THR_LO + ALARM_EXT_TEMP_1_HST) &&
 		    g_app_sensor_data.ext_temperature_1 <
-			    (ALARM_EXT_TEMPERATURE_1_THR_HI - ALARM_EXT_TEMPERATURE_1_HST)) {
+			    (ALARM_EXT_TEMP_1_THR_HI - ALARM_EXT_TEMP_1_HST)) {
 			LOG_INF("Deactivated alarm for external temperature 1");
 
-			alarm_ext_temperature_1 = false;
+			alarm_ext_temp_1 = false;
 		}
 	} else {
 		if (g_app_sensor_data.ext_temperature_1 <
-			    (ALARM_EXT_TEMPERATURE_1_THR_LO - ALARM_EXT_TEMPERATURE_1_HST) ||
+			    (ALARM_EXT_TEMP_1_THR_LO - ALARM_EXT_TEMP_1_HST) ||
 		    g_app_sensor_data.ext_temperature_1 >
-			    (ALARM_EXT_TEMPERATURE_1_THR_HI + ALARM_EXT_TEMPERATURE_1_HST)) {
+			    (ALARM_EXT_TEMP_1_THR_HI + ALARM_EXT_TEMP_1_HST)) {
 			LOG_INF("Activated alarm for external temperature 1");
 
-			alarm_ext_temperature_1 = true;
+			alarm_ext_temp_1 = true;
 		}
 	}
 
-	static bool alarm_ext_temperature_2 = false;
+	static bool alarm_ext_temp_2 = false;
 
 	if (isnan(g_app_sensor_data.ext_temperature_2)) {
-		alarm_ext_temperature_2 = false;
-	} else if (alarm_ext_temperature_2) {
+		alarm_ext_temp_2 = false;
+	} else if (alarm_ext_temp_2) {
 		if (g_app_sensor_data.ext_temperature_2 >
-			    (ALARM_EXT_TEMPERATURE_2_THR_LO + ALARM_EXT_TEMPERATURE_2_HST) &&
+			    (ALARM_EXT_TEMP_2_THR_LO + ALARM_EXT_TEMP_2_HST) &&
 		    g_app_sensor_data.ext_temperature_2 <
-			    (ALARM_EXT_TEMPERATURE_2_THR_HI - ALARM_EXT_TEMPERATURE_2_HST)) {
+			    (ALARM_EXT_TEMP_2_THR_HI - ALARM_EXT_TEMP_2_HST)) {
 			LOG_INF("Deactivated alarm for external temperature 2");
 
-			alarm_ext_temperature_2 = false;
+			alarm_ext_temp_2 = false;
 		}
 	} else {
 		if (g_app_sensor_data.ext_temperature_2 <
-			    (ALARM_EXT_TEMPERATURE_2_THR_LO - ALARM_EXT_TEMPERATURE_2_HST) ||
+			    (ALARM_EXT_TEMP_2_THR_LO - ALARM_EXT_TEMP_2_HST) ||
 		    g_app_sensor_data.ext_temperature_2 >
-			    (ALARM_EXT_TEMPERATURE_2_THR_HI + ALARM_EXT_TEMPERATURE_2_HST)) {
+			    (ALARM_EXT_TEMP_2_THR_HI + ALARM_EXT_TEMP_2_HST)) {
 			LOG_INF("Activated alarm for external temperature 2");
 
-			alarm_ext_temperature_2 = true;
+			alarm_ext_temp_2 = true;
 		}
 	}
 
-	return alarm_temperature || alarm_ext_temperature_1 || alarm_ext_temperature_2;
+	bool alarm = false;
+
+#if defined(CONFIG_APP_ALARM_INT_TEMP)
+	alarm = alarm_int_temp ? true : alarm;
+#endif /* defined(CONFIG_APP_ALARM_INT_TEMP) */
+
+#if defined(CONFIG_APP_ALARM_EXT_TEMP)
+	alarm = alarm_ext_temp_1 ? true : alarm;
+	alarm = alarm_ext_temp_2 ? true : alarm;
+#endif /* defined(CONFIG_APP_ALARM_EXT_TEMP) */
+
+	return alarm;
 }
