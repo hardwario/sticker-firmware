0cb8b00373a21ef60ab750b675623f9a33a6b3b0
Pavel Hübner <pavel.hubner@hardwario.com>
2025-09-29 13:55:46 +0200

    Apply LoRaWAN parameters from config

--- app/src/app_lrw.c
+++ app/src/app_lrw.c
@@ -24,8 +24,8 @@
 
 LOG_MODULE_REGISTER(app_lrw, LOG_LEVEL_DBG);
 
-static void downlink_cb(uint8_t port, uint8_t flags, int16_t rssi, int8_t snr, uint8_t len,
-			const uint8_t *data)
+static void downlink_callback(uint8_t port, uint8_t flags, int16_t rssi, int8_t snr, uint8_t len,
+			      const uint8_t *data)
 {
 	LOG_INF("Port %d, Flags 0x%02x, RSSI %d dB, SNR %d dBm", port, flags, rssi, snr);
 
@@ -34,13 +34,13 @@ static void downlink_cb(uint8_t port, uint8_t flags, int16_t rssi, int8_t snr, u
 	}
 }
 
-static uint8_t battery_level_cb(void)
+static uint8_t battery_level_callback(void)
 {
 	/* TODO Implement */
 	return 255;
 }
 
-static void datarate_changed_cb(enum lorawan_datarate dr)
+static void datarate_changed_callback(enum lorawan_datarate dr)
 {
 	uint8_t max_next_payload_size;
 	uint8_t max_payload_size;
@@ -56,17 +56,32 @@ int app_lrw_join(void)
 
 	static struct lorawan_join_config config = {0};
 
-	config.mode = LORAWAN_ACT_ABP;
 	config.dev_eui = g_app_config.lrw_deveui;
-	config.abp.dev_addr = sys_get_be32(g_app_config.lrw_devaddr);
-	config.abp.nwk_skey = g_app_config.lrw_nwkskey;
-	config.abp.app_skey = g_app_config.lrw_appskey;
+
+	if (g_app_config.lrw_activation == APP_CONFIG_LRW_ACTIVATION_OTAA) {
+		LOG_INF("Using OTAA activation");
+		config.mode = LORAWAN_ACT_OTAA;
+		config.otaa.join_eui = g_app_config.lrw_joineui;
+		config.otaa.nwk_key = g_app_config.lrw_nwkkey;
+		config.otaa.app_key = g_app_config.lrw_appkey;
+	} else if (g_app_config.lrw_activation == APP_CONFIG_LRW_ACTIVATION_ABP) {
+		LOG_INF("Using ABP activation");
+		config.mode = LORAWAN_ACT_ABP;
+		config.abp.dev_addr = sys_get_be32(g_app_config.lrw_devaddr);
+		config.abp.nwk_skey = g_app_config.lrw_nwkskey;
+		config.abp.app_skey = g_app_config.lrw_appskey;
+	} else {
+		LOG_ERR("Invalid activation mode: %d", g_app_config.lrw_activation);
+		return -EINVAL;
+	}
 
 	ret = lorawan_join(&config);
 	if (ret) {
 		LOG_ERR("Call `lorawan_join` failed: %d", ret);
 		return ret;
 	}
+
+	lorawan_enable_adr(g_app_config.lrw_adr);
 
 	return 0;
 }
@@ -94,13 +109,28 @@ static int init(void)
 		return -ENODEV;
 	}
 
-#if defined(CONFIG_LORAMAC_REGION_EU868)
-	ret = lorawan_set_region(LORAWAN_REGION_EU868);
+	enum lorawan_region region;
+
+	switch (g_app_config.lrw_region) {
+	case APP_CONFIG_LRW_REGION_EU868:
+		region = LORAWAN_REGION_EU868;
+		break;
+	case APP_CONFIG_LRW_REGION_US915:
+		region = LORAWAN_REGION_US915;
+		break;
+	case APP_CONFIG_LRW_REGION_AU915:
+		region = LORAWAN_REGION_AU915;
+		break;
+	default:
+		LOG_ERR("Invalid region: %d", g_app_config.lrw_region);
+		return -EINVAL;
+	}
+
+	ret = lorawan_set_region(region);
 	if (ret) {
 		LOG_ERR("Call `lorawan_set_region` failed: %d", ret);
 		return ret;
 	}
-#endif /* defined(CONFIG_LORAMAC_REGION_EU868) */
 
 	ret = lorawan_start();
 	if (ret) {
@@ -108,14 +138,14 @@ static int init(void)
 		return ret;
 	}
 
-	static struct lorawan_downlink_cb cb = {
+	static struct lorawan_downlink_cb downlink_cb = {
 		.port = LW_RECV_PORT_ANY,
-		.cb = downlink_cb,
+		.cb = downlink_callback,
 	};
 
-	lorawan_register_downlink_callback(&cb);
-	lorawan_register_battery_level_callback(battery_level_cb);
-	lorawan_register_dr_changed_callback(datarate_changed_cb);
+	lorawan_register_downlink_callback(&downlink_cb);
+	lorawan_register_battery_level_callback(battery_level_callback);
+	lorawan_register_dr_changed_callback(datarate_changed_callback);
 
 	return 0;
 }
