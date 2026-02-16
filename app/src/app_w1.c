7f6d83b8dee2e6bace6965936354eaccc62a4909
Pavel Hübner <pavel.hubner@hardwario.com>
2026-02-16 23:19:51 +0800

    Fix app_w1_acquire unlocking bus on lock failure
    Return immediately when w1_lock_bus fails instead of jumping to
    the error label which calls w1_unlock_bus on a lock never acquired.

--- app/src/app_w1.c
+++ app/src/app_w1.c
@@ -41,8 +41,7 @@ int app_w1_acquire(struct app_w1 *w1, const struct device *dev)
 	ret = w1_lock_bus(dev);
 	if (ret) {
 		LOG_ERR_CALL_FAILED_INT("w1_lock_bus", ret);
-		res = ret;
-		goto error;
+		return ret;
 	}
 
 	ret = pm_device_action_run(dev, PM_DEVICE_ACTION_RESUME);
